1a34401fceac8afe3bffbca1fef26c445afc5e31
TOAN NGUYEN <ttngbt06@gmail.com>
2024-04-05 01:52:34 -0500

    Create Portfolio page

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,192 @@
+function PortfolioPage() {
+    return (
+      <section class="bg-white dark:bg-gray-900 antialiased">
+        <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+          <div class="max-w-2xl mx-auto text-center">
+            <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+              Portfolio
+            </h2>
+            <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
+              These Are Some Projects I've done
+            </p>
+          </div>
+  
+          <div class="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Eat & Stay Planning Website
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/JacobK05/project_1/raw/develop/assets/Images/screenshot.png"
+              alt="project 1"></img>
+            <a href="https://jacobk05.github.io/project_1/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/JacobK05/project_1" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+          
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Weather Dashboard
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-Weather-Dashboard/raw/main/assets/Image/demo.png"
+              alt="project 1"></img>
+            <a href="https://ttngbt06.github.io/TN-Weather-Dashboard/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-Weather-Dashboard" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              JS Coding Quiz
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/Challenge_Module4/raw/main/Assets/MockUp_Challenge4.gif"
+              alt="office content 1"></img>
+            <a href="https://ttngbt06.github.io/Challenge_Module4/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-JS-Code-Quiz" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Work Day Scheduler
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-Work-Day-Scheduler/blob/main/Assets/Image/MockUpChal5.gif?raw=true"
+              alt="project 1"></img>
+            <a href="https://ttngbt06.github.io/TN-Work-Day-Scheduler/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-Work-Day-Scheduler" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Note Taker
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-Note-Taker/blob/main/public/assets/image/11-express-homework-demo.gif?raw=true"
+              alt="project 1"></img>
+            <a href="https://tn-note-taker-0b5b741b4c9f.herokuapp.com/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-Note-Taker" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Tech Blog
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-Tech-Blog/blob/main/images/techblogdemo.gif?raw=true"
+              alt="project 1"></img>
+            <a href="https://tn-tech-blog-28e7ff7addba.herokuapp.com/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-Tech-Blog" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Social Network API
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-NoSQL-SocialNetworkAPI/blob/main/Screenshot.gif?raw=true"
+              alt="project 1"></img>
+            <a href="https://github.com/ttngbt06/TN-NoSQL-SocialNetworkAPI" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+          <div class="space-y-4">
+            <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+              Just Another Text Taker
+            </h3>
+            <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+              {/* add description later */}
+            </p>
+            <img class="w-full rounded-lg"
+              src="https://github.com/ttngbt06/TN-PWA-JATE/blob/main/assets/screenshot.png?raw=true"
+              alt="project 1"></img>
+            <a href="https://tn-pwa-jate.onrender.com/" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              View deployed site
+            </a>
+            <a href="https://github.com/ttngbt06/TN-PWA-JATE" title="" target="_blank"
+              class="text-gray-500 bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 md:hover:text-blue-700"
+              role="button">
+              Visit GitHub repository
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default PortfolioPage;
