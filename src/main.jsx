426d4de9eaffcd18a484bf3af656d46973b709db
TOAN NGUYEN <ttngbt06@gmail.com>
2024-04-05 21:35:15 -0500

    Set up app

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx'
+import './index.css'
+
+import AboutPage from './pages/AboutPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
